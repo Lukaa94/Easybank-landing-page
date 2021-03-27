d7d6354e03f1b1673efdb5a161478176318e4ed3
Luka Juric <lukajuric98765@gmail.com>
2021-03-27 22:54:15 +0100

    fix naming

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const navbar = document.querySelector(".navbar");
 const overlay = document.querySelector(".overlay");
-const hamburger = document.querySelector(".navbar-hamburger");
-const closeBtn = document.querySelector(".navbar-close");
+const hamburger = document.querySelector(".hamburger-icon");
+const xIcon = document.querySelector(".x-icon");
 
 
 hamburger.addEventListener("click", () =>{
     navbar.classList.remove("hide");
     overlay.classList.remove("hide");
-    closeBtn.classList.remove("hide");
+    xIcon.classList.remove("hide");
     hamburger.classList.add("hide");
 });
 
-closeBtn.addEventListener("click", () =>{
+xIcon.addEventListener("click", () =>{
     navbar.classList.add("hide");
     overlay.classList.add("hide");
-    closeBtn.classList.add("hide");
+    xIcon.classList.add("hide");
     hamburger.classList.remove("hide");
 });
